dc6e766cca759745abbdd101fa85960f827fa512
Juan Pablo Peretti <jpp@Juans-MacBook-Pro.local>
2019-01-24 11:36:18 -0300

    Adding espresso dependencies 1

---
+++ app/src/androidTest/java/com/jpp/moviespreview/screens/main/search/SearchFragmentTest.kt
@@ -0,0 +1,18 @@
+package com.jpp.moviespreview.screens.main.search
+
+
+
+import androidx.arch.core.executor.testing.CountingTaskExecutorRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class SearchFragmentTest {
+
+
+    @get:Rule
+    var testRule = CountingTaskExecutorRule()
+
+
+}
