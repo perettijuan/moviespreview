d12bc83fc443ed0a10ef10dbc3d75b11ec6c607b
Juan Pablo Peretti <jpp@Juans-MacBook-Pro.local>
2019-01-24 14:15:39 -0300

    Update emulators for travis build

--- app/src/androidTest/java/com/jpp/moviespreview/screens/main/search/SearchFragmentTest.kt
+++ app/src/androidTest/java/com/jpp/moviespreview/screens/main/search/SearchFragmentTest.kt
@@ -11,8 +11,19 @@ import org.junit.runner.RunWith
 class SearchFragmentTest {
 
 
+    //TODO JPP do I need this?
     @get:Rule
     var testRule = CountingTaskExecutorRule()
+
+    /*
+     * Using https://github.com/SabagRonen/dagger-activity-test-sample/blob/ActivitySample/app/src/androidTest/java/com/example/ronensabag/daggeractivitytestsample/MainViewTests.kt
+     * (from https://proandroiddev.com/activity-espresso-test-with-daggers-android-injector-82f3ee564aa4)
+     * 1 - Inject a mock ViewModelProvider.Factory.
+     * 2 - The mock will provide an instance of each ViewModel (not a mock).
+     * 3 - Each ViewModel will have a mock of the Repository.
+     * 4 - Before executing a test, we need to navigate to the given fragment and wait for that
+     *     fragment to be visible.
+     */
 
 
 }
