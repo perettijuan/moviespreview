81c27854dd4c20b604574eeae41fef1f58f1b71b
Juan Pablo Peretti <peretti.juan@gmail.com>
2019-03-01 19:00:27 -0300

    Move classes to the right place 2

--- app/src/main/java/com/jpp/moviespreview/screens/main/main_model.kt
+++ app/src/main/java/com/jpp/moviespreview/screens/main/main_model.kt
@@ -1,5 +1,15 @@
 package com.jpp.moviespreview.screens.main
 
+/**
+ * Represents the view state that the MainActivity can show at any given time.
+ */
+sealed class MainActivityViewState(val sectionTitle: String, val menuBarEnabled: Boolean) {
+    data class ActionBarLocked(val abTitle: String, val withAnimation: Boolean, val searchEnabled: Boolean) : MainActivityViewState(abTitle, searchEnabled)
+    data class ActionBarUnlocked(val abTitle: String, val contentImageUrl: String) : MainActivityViewState(abTitle, false)
+    data class SearchEnabled(val withAnimation: Boolean) : MainActivityViewState("", false)
+}
+
+
 sealed class SearchEvent {
     object ClearSearch : SearchEvent()
     data class Search(val query: String) : SearchEvent()
