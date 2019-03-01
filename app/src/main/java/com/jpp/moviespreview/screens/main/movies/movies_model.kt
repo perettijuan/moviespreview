ca698ae16bfc98ec5e55c30cbe225a9be8bbb046
Juan Pablo Peretti <peretti.juan@gmail.com>
2019-03-01 18:59:45 -0300

    Move classes to the right place

--- app/src/main/java/com/jpp/moviespreview/screens/main/movies/movies_model.kt
+++ app/src/main/java/com/jpp/moviespreview/screens/main/movies/movies_model.kt
@@ -2,10 +2,28 @@ package com.jpp.moviespreview.screens.main.movies
 
 import com.jpp.mpdomain.Movie as DomainMovie
 
-/***************************************************************************************************
- ********* Contains all the model classes that the movies section in the UI layer supports. ********
- ***************************************************************************************************/
+import androidx.paging.PagedList
 
+/**
+ * Represents the view state of the movies view (MoviesFragment).
+ * Each subclass of this sealed class represents a particular state that the fragment
+ * can assume.
+ */
+sealed class MoviesViewState {
+    object Loading : MoviesViewState()
+    object ErrorNoConnectivity : MoviesViewState()
+    object ErrorNoConnectivityWithItems : MoviesViewState()
+    object ErrorUnknown : MoviesViewState()
+    object ErrorUnknownWithItems : MoviesViewState()
+    data class InitialPageLoaded(val pagedList: PagedList<MovieItem>) : MoviesViewState()
+}
+
+/**
+ * Represents the navigation events that can be routed through the search section.
+ */
+sealed class MoviesViewNavigationEvent {
+    data class ToMovieDetails(val movieId: String, val movieImageUrl: String, val movieTitle: String) : MoviesViewNavigationEvent()
+}
 /**
  * Represents an item in the list of Movies shown in the initial screen of the application.
  */
