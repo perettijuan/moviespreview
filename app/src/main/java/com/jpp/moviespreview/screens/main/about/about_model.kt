ce79c15fce2d1660212ff957eb899777beecf2fe
Juan Pablo Peretti <peretti.juan@gmail.com>
2019-03-08 14:57:27 -0300

    Added about fragment   layout

---
+++ app/src/main/java/com/jpp/moviespreview/screens/main/about/about_model.kt
@@ -0,0 +1,16 @@
+package com.jpp.moviespreview.screens.main.about
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.jpp.moviespreview.R
+
+/**
+ * Represents a selectable action in the about section.
+ */
+sealed class AboutItem(@StringRes val title: Int, @DrawableRes val icon: Int) {
+    object RateApp : AboutItem(R.string.about_rate_app_action, R.drawable.ic_rate_app)
+    object ShareApp : AboutItem(R.string.about_share_app_action, R.drawable.ic_share_app)
+    object BrowseAppCode : AboutItem(R.string.about_brows_code_action, R.drawable.ic_github_logo)
+    object Licenses : AboutItem(R.string.about_open_source_action, R.drawable.ic_open_source_libraries)
+    object TheMovieDbTermsOfUse : AboutItem(R.string.about_the_movie_db_terms_of_use, R.drawable.ic_the_movie_db)
+}
