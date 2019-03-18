bae6d23d8a5c24e513d0e1d0ee8c704f725acd9a
Juan Pablo Peretti <peretti.juan@gmail.com>
2019-03-18 14:54:28 -0300

    Add animation for search list

--- app/src/main/java/com/jpp/moviespreview/screens/main/search/SearchFragment.kt
+++ app/src/main/java/com/jpp/moviespreview/screens/main/search/SearchFragment.kt
@@ -194,9 +194,8 @@ class SearchFragment : Fragment() {
         searchErrorView.setInvisible()
         searchLoadingView.setInvisible()
         searchResultRv.setInvisible()
-
+        
         searchPlaceHolderIv.setVisible()
-        searchPlaceHolderIv.alpha = 0.3F
     }
 
     private fun renderSearching() {
@@ -224,6 +223,7 @@ class SearchFragment : Fragment() {
         searchLoadingView.setInvisible()
 
         searchResultRv.setVisible()
+        searchResultRv.scheduleLayoutAnimation()
     }
 
     private fun renderEmptySearch() {
