27489748ef755c39d53b64a22c3e2489341b0739
Juan Pablo Peretti <peretti.juan@gmail.com>
2019-03-25 18:01:45 -0300

    Update GetAccessTokenUseCaseto return URL

--- mpdata/src/main/java/com/jpp/mpdata/repository/account/SessionRepositoryImpl.kt
+++ mpdata/src/main/java/com/jpp/mpdata/repository/account/SessionRepositoryImpl.kt
@@ -4,11 +4,25 @@ import com.jpp.mpdomain.AccessToken
 import com.jpp.mpdomain.repository.SessionRepository
 
 class SessionRepositoryImpl(private val sessionApi: SessionApi) : SessionRepository {
+
     override fun getSessionId(): String? {
         return null
     }
 
     override fun getAccessToken(): AccessToken? {
         return sessionApi.getAccessToken()
+    }
+
+    override fun getAuthenticationUrl(accessToken: String): String {
+        return "$authUrl/$accessToken?redirect_to=$redirectUrl"
+    }
+
+    override fun getAuthenticationRedirection(): String {
+       return redirectUrl
+    }
+
+    private companion object {
+        const val authUrl = "https://www.themoviedb.org/authenticate/"
+        const val redirectUrl = "http://www.mp.com/approved"
     }
 }
