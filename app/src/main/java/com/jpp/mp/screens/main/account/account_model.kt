78a7687acd6266077736cc0e086b8850e8933ad5
Juan Pablo Peretti <peretti.juan@gmail.com>
2019-03-28 14:58:01 -0300

    Create session done

--- app/src/main/java/com/jpp/mp/screens/main/account/account_model.kt
+++ app/src/main/java/com/jpp/mp/screens/main/account/account_model.kt
@@ -1,5 +1,7 @@
 package com.jpp.mp.screens.main.account
 
+import com.jpp.mpdomain.AccessToken
+
 /**
  * Represents all the view states that the AccountFragment can assume.
  */
@@ -7,5 +9,6 @@ sealed class AccountViewState {
     object Loading : AccountViewState()
     object ErrorUnknown : AccountViewState()
     object ErrorNoConnectivity : AccountViewState()
-    data class RenderlURL(val url: String, val interceptUrl: String) : AccountViewState()
+    object SessionCreated : AccountViewState()// TODO JPP this should be another class that represents the user account data
+    data class RenderlURL(val url: String, val interceptUrl: String, val accessToken: AccessToken) : AccountViewState()
 }
