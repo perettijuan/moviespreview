07bbf0bea9237a8bc1afd68fdceb3531624fe700
Juan Pablo Peretti <peretti.juan@gmail.com>
2019-03-29 11:13:48 -0300

    Handle account info in memory

--- mpdata/src/main/java/com/jpp/mpdata/repository/account/AccountRepositoryImpl.kt
+++ mpdata/src/main/java/com/jpp/mpdata/repository/account/AccountRepositoryImpl.kt
@@ -4,7 +4,15 @@ import com.jpp.mpdomain.Session
 import com.jpp.mpdomain.UserAccount
 import com.jpp.mpdomain.repository.AccountRepository
 
-class AccountRepositoryImpl(private val accountApi: AccountApi) : AccountRepository {
-    //TODO use in memory caching
-    override fun getUserAccount(session: Session): UserAccount? = accountApi.getUserAccountInfo(session)
+class AccountRepositoryImpl(private val accountApi: AccountApi,
+                            private val accountDb: AccountDb) : AccountRepository {
+
+
+    override fun getUserAccount(session: Session): UserAccount? {
+        return accountDb.getUserAccountInfo() ?: run {
+            accountApi.getUserAccountInfo(session)?.also {
+                accountDb.storeUserAccountInfo(it)
+            }
+        }
+    }
 }
