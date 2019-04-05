18a791033316f0a40cfe0ea2e6e6c4cbadb184a6
Juan Pablo Peretti <peretti.juan@gmail.com>
2019-04-05 17:09:11 -0300

    Updated MovieActionsViewModelTest

--- app/src/test/java/com/jpp/mp/screens/main/details/MovieActionsViewModelTest.kt
+++ app/src/test/java/com/jpp/mp/screens/main/details/MovieActionsViewModelTest.kt
@@ -6,6 +6,7 @@ import com.jpp.mp.resumedLifecycleOwner
 import com.jpp.mp.screens.main.TestCoroutineDispatchers
 import com.jpp.mpdomain.MovieAccountState
 import com.jpp.mpdomain.usecase.details.GetMovieAccountStateUseCase
+import com.jpp.mpdomain.usecase.movies.MarkMovieAsFavoriteUseCase
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
@@ -23,6 +24,8 @@ class MovieActionsViewModelTest {
 
     @MockK
     private lateinit var getMovieAccountStateUseCase: GetMovieAccountStateUseCase
+    @MockK
+    private lateinit var favoriteMovieUseCase: MarkMovieAsFavoriteUseCase
 
     private lateinit var subject: MovieActionsViewModel
 
@@ -30,9 +33,12 @@ class MovieActionsViewModelTest {
 
     @BeforeEach
     fun setUp() {
-        subject = MovieActionsViewModel(TestCoroutineDispatchers(), getMovieAccountStateUseCase)
+        subject = MovieActionsViewModel(
+                TestCoroutineDispatchers(),
+                getMovieAccountStateUseCase,
+                favoriteMovieUseCase
+        )
     }
-
 
     @ParameterizedTest
     @MethodSource("executeParameters")
@@ -51,6 +57,35 @@ class MovieActionsViewModelTest {
         assertTrue(viewStatePosted[0] is MovieActionsState.Hidden)
         assertEquals(viewStatePosted[1], expectedState)
         verify(exactly = 1) { getMovieAccountStateUseCase.getMovieAccountState(movieDetailId) }
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("favTestParameters")
+    fun `Should push updating favorite and update favorite state`(initialFavState: Boolean,
+                                                                  favUcCaseStateCall: Boolean,
+                                                                  favUcResult: MarkMovieAsFavoriteUseCase.FavoriteMovieResult,
+                                                                  expectedFinalState: MovieActionsState) {
+        // First, set the VM in the proper state
+        val ucResponse = GetMovieAccountStateUseCase.MovieAccountStateResult.Success(
+                MovieAccountState(121.toDouble(), favorite = initialFavState, rated = false, watchlist = false))
+        every { getMovieAccountStateUseCase.getMovieAccountState(any()) } returns ucResponse
+
+        subject.init(movieDetailId)
+
+        every { favoriteMovieUseCase.favoriteMovie(any(), any()) } returns favUcResult
+
+        val viewStatePosted = mutableListOf<MovieActionsState>()
+
+        subject.actionsState().observe(resumedLifecycleOwner(), Observer {
+            viewStatePosted.add(it)
+        })
+
+        subject.updateMovieFavoriteState(movieDetailId)
+
+        assertEquals(MovieActionsState.Updating(favorite = true), viewStatePosted[1]) // one b/c the first posted is the result of init call
+        assertEquals(expectedFinalState, viewStatePosted[2])
+        verify { favoriteMovieUseCase.favoriteMovie(movieDetailId, favUcCaseStateCall) }
     }
 
     companion object {
@@ -77,6 +112,41 @@ class MovieActionsViewModelTest {
                 Arguments.of(
                         GetMovieAccountStateUseCase.MovieAccountStateResult.Success(
                                 MovieAccountState(121.toDouble(), favorite = false, rated = false, watchlist = false)),
+                        MovieActionsState.Shown(isFavorite = false)
+                )
+        )
+
+
+        @JvmStatic
+        fun favTestParameters() = listOf(
+                Arguments.of(
+                        true,
+                        false,
+                        MarkMovieAsFavoriteUseCase.FavoriteMovieResult.Success,
+                        MovieActionsState.Shown(isFavorite = false)
+                ),
+                Arguments.of(
+                        false,
+                        true,
+                        MarkMovieAsFavoriteUseCase.FavoriteMovieResult.Success,
+                        MovieActionsState.Shown(isFavorite = true)
+                ),
+                Arguments.of(
+                        false,
+                        true,
+                        MarkMovieAsFavoriteUseCase.FavoriteMovieResult.UserNotLogged,
+                        MovieActionsState.UserNotLoggedIn
+                ),
+                Arguments.of(
+                        false,
+                        true,
+                        MarkMovieAsFavoriteUseCase.FavoriteMovieResult.ErrorUnknown,
+                        MovieActionsState.Shown(isFavorite = false)
+                ),
+                Arguments.of(
+                        false,
+                        true,
+                        MarkMovieAsFavoriteUseCase.FavoriteMovieResult.ErrorNoConnectivity,
                         MovieActionsState.Shown(isFavorite = false)
                 )
         )
