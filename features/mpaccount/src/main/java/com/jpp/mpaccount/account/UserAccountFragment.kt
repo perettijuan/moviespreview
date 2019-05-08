271c36c37654cf00cf18210d1108778d2a81c8ea
Juan Pablo Peretti <peretti.juan@gmail.com>
2019-05-08 15:52:52 -0300

    Navigate to login fragment

--- features/mpaccount/src/main/java/com/jpp/mpaccount/account/UserAccountFragment.kt
+++ features/mpaccount/src/main/java/com/jpp/mpaccount/account/UserAccountFragment.kt
@@ -5,10 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.jpp.mpaccount.R
+import kotlinx.android.synthetic.main.fragment_user_account.*
 
 class UserAccountFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_user_account, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        //TODO JPP this needs to be done based on the inner app state
+        loginButton.setOnClickListener {
+            findNavController().navigate(R.id.toLoginFragment)
+        }
     }
 }
---
+++ features/mpaccount/src/main/java/com/jpp/mpaccount/login/LoginFragment.kt
@@ -0,0 +1,15 @@
+package com.jpp.mpaccount.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.jpp.mpaccount.R
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+}
