e292336ea700f886d922820be9f04ea94ccf405a
Juan Pablo Peretti <peretti.juan@gmail.com>
2019-06-13 16:06:48 -0300

    Fix failling tests

--- app/src/test/java/com/jpp/mp/screens/main/MainActivityViewModelTest.kt
+++ app/src/test/java/com/jpp/mp/screens/main/MainActivityViewModelTest.kt
@@ -12,6 +12,9 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 @ExtendWith(MockKExtension::class, InstantTaskExecutorExtension::class)
 class MainActivityViewModelTest {
@@ -30,175 +33,14 @@ class MainActivityViewModelTest {
         subject = MainActivityViewModel(connectivityMonitor, languageMonitor, languageRepository)
     }
 
-    @Test
-    fun `Should lock ActionBar without animation when user navigates to movies list`() {
-        subject.viewState().observeWith {
-            assertTrue(it is MainActivityViewState.ActionBarLocked)
-            with(it as MainActivityViewState.ActionBarLocked) {
-                assertEquals("aSection", abTitle)
-                assertFalse(withAnimation)
-                assertTrue(menuEnabled)
-                assertFalse(searchEnabled)
-            }
-        }
-        subject.userNavigatesToMovieListSection("aSection")
+
+    @ParameterizedTest
+    @MethodSource("navigationEvents")
+    fun `Should post navigation event`(param: NavigationEventsParams) {
+        subject.viewState().observeWith { assertEquals(param.expected, it) }
+        param.action.invoke(subject)
     }
 
-    @Test
-    fun `Should lock ActionBar with animation when user navigates to movies list back from details`() {
-        val viewStatesPosted = mutableListOf<MainActivityViewState>()
-        // navigate to details to pre-set the ActionBar state as unlocked
-        subject.userNavigatesToMovieDetails(movieTitle = "aTitle", contentImageUrl = "aUrl")
-
-        subject.viewState().observeWith { viewStatesPosted.add(it) }
-
-        subject.userNavigatesToMovieListSection("aSection")
-
-        assertTrue(viewStatesPosted[1] is MainActivityViewState.ActionBarLocked)
-        with(viewStatesPosted[1] as MainActivityViewState.ActionBarLocked) {
-            assertEquals("aSection", abTitle)
-            assertTrue(withAnimation)
-            assertTrue(menuEnabled)
-            assertFalse(searchEnabled)
-        }
-    }
-
-    @Test
-    fun `Should unlock ActionBar when user navigates to details`() {
-        subject.viewState().observeWith {
-            assertTrue(it is MainActivityViewState.ActionBarUnlocked)
-            with(it as MainActivityViewState.ActionBarUnlocked) {
-                assertEquals("aTitle", abTitle)
-                assertEquals("aUrl", contentImageUrl)
-                assertFalse(searchEnabled)
-            }
-        }
-        subject.userNavigatesToMovieDetails(movieTitle = "aTitle", contentImageUrl = "aUrl")
-    }
-
-    @Test
-    fun `Should navigate to search section without ActionBar animation when user opens search`() {
-        subject.viewState().observeWith {
-            assertTrue(it is MainActivityViewState.ActionBarLocked)
-            with(it as MainActivityViewState.ActionBarLocked) {
-                assertFalse(withAnimation)
-                assertTrue(searchEnabled)
-            }
-        }
-        subject.userNavigatesToSearch()
-    }
-
-    @Test
-    fun `Should navigate to search section with ActionBar animation when user navigates to search back from details`() {
-        val viewStatesPosted = mutableListOf<MainActivityViewState>()
-        // navigate to details to pre-set the ActionBar state as unlocked
-        subject.userNavigatesToMovieDetails(movieTitle = "aTitle", contentImageUrl = "aUrl")
-
-        subject.viewState().observeWith { viewStatesPosted.add(it) }
-
-        subject.userNavigatesToSearch()
-
-        assertTrue(viewStatesPosted[1] is MainActivityViewState.ActionBarLocked)
-        with(viewStatesPosted[1] as MainActivityViewState.ActionBarLocked) {
-            assertTrue(withAnimation)
-            assertTrue(searchEnabled)
-        }
-    }
-
-    @Test
-    fun `Should lock ActionBar without animation when user navigates to movies person`() {
-        val personName = "aPerson"
-        subject.viewState().observeWith {
-            assertTrue(it is MainActivityViewState.ActionBarLocked)
-            with(it as MainActivityViewState.ActionBarLocked) {
-                assertEquals(personName, abTitle)
-                assertFalse(withAnimation)
-                assertFalse(menuEnabled)
-                assertFalse(searchEnabled)
-            }
-        }
-        subject.userNavigatesToPerson(personName)
-    }
-
-
-    @Test
-    fun `Should lock ActionBar with animation when user navigates to movies credits`() {
-        val viewStatesPosted = mutableListOf<MainActivityViewState>()
-        // navigate to details to pre-set the ActionBar state as unlocked
-        subject.userNavigatesToMovieDetails(movieTitle = "aTitle", contentImageUrl = "aUrl")
-
-        val creditsName = "credits"
-        subject.viewState().observeWith { viewStatesPosted.add(it) }
-        subject.userNavigatesToCredits(creditsName)
-
-        assertTrue(viewStatesPosted[1] is MainActivityViewState.ActionBarLocked)
-        with(viewStatesPosted[1] as MainActivityViewState.ActionBarLocked) {
-            assertEquals(creditsName, abTitle)
-            assertTrue(withAnimation)
-            assertFalse(menuEnabled)
-            assertFalse(searchEnabled)
-        }
-    }
-
-    @Test
-    fun `Should lock ActionBar without animation when user navigates to movies about`() {
-        val sectionName = "aSection"
-        subject.viewState().observeWith {
-            assertTrue(it is MainActivityViewState.ActionBarLocked)
-            with(it as MainActivityViewState.ActionBarLocked) {
-                assertEquals(sectionName, abTitle)
-                assertFalse(withAnimation)
-                assertFalse(menuEnabled)
-                assertFalse(searchEnabled)
-            }
-        }
-        subject.userNavigatesToAbout(sectionName)
-    }
-
-    @Test
-    fun `Should lock ActionBar without animation when user navigates to movies licenses`() {
-        val sectionName = "aSection"
-        subject.viewState().observeWith {
-            assertTrue(it is MainActivityViewState.ActionBarLocked)
-            with(it as MainActivityViewState.ActionBarLocked) {
-                assertEquals(sectionName, abTitle)
-                assertFalse(withAnimation)
-                assertFalse(menuEnabled)
-                assertFalse(searchEnabled)
-            }
-        }
-        subject.userNavigatesToLicenses(sectionName)
-    }
-
-    @Test
-    fun `Should lock ActionBar without animation when user navigates to movies licenses content`() {
-        val sectionName = "aSection"
-        subject.viewState().observeWith {
-            assertTrue(it is MainActivityViewState.ActionBarLocked)
-            with(it as MainActivityViewState.ActionBarLocked) {
-                assertEquals(sectionName, abTitle)
-                assertFalse(withAnimation)
-                assertFalse(menuEnabled)
-                assertFalse(searchEnabled)
-            }
-        }
-        subject.userNavigatesToLicenseContent(sectionName)
-    }
-
-    @Test
-    fun `Should lock ActionBar without animation when user navigates to account details`() {
-        val sectionName = "aSection"
-        subject.viewState().observeWith {
-            assertTrue(it is MainActivityViewState.ActionBarLocked)
-            with(it as MainActivityViewState.ActionBarLocked) {
-                assertEquals(sectionName, abTitle)
-                assertFalse(withAnimation)
-                assertFalse(menuEnabled)
-                assertFalse(searchEnabled)
-            }
-        }
-        subject.userNavigatesToAccountDetails(sectionName)
-    }
 
     @Test
     fun `Should start monitoring in onInit`() {
@@ -213,5 +55,53 @@ class MainActivityViewModelTest {
         subject.onInit()
 
         verify { languageRepository.syncPlatformLanguage() }
+    }
+
+
+    data class NavigationEventsParams(
+            val expected: MainActivityViewState,
+            val action: (MainActivityViewModel) -> Unit
+    )
+
+    companion object {
+        @JvmStatic
+        fun navigationEvents() = listOf(
+                NavigationEventsParams(
+                        MainActivityViewState(sectionTitle = "aSection", menuBarEnabled = false, searchEnabled = false),
+                        action = { it.userNavigatesToMovieListSection("aSection") }
+                ),
+                NavigationEventsParams(
+                        MainActivityViewState(sectionTitle = "aSection", menuBarEnabled = false, searchEnabled = false),
+                        action = { it.userNavigatesToMovieDetails("aSection") }
+                ),
+                NavigationEventsParams(
+                        MainActivityViewState(sectionTitle = "", menuBarEnabled = false, searchEnabled = true),
+                        action = { it.userNavigatesToSearch() }
+                ),
+                NavigationEventsParams(
+                        MainActivityViewState(sectionTitle = "aSection", menuBarEnabled = false, searchEnabled = false),
+                        action = { it.userNavigatesToCredits("aSection") }
+                ),
+                NavigationEventsParams(
+                        MainActivityViewState(sectionTitle = "aSection", menuBarEnabled = false, searchEnabled = false),
+                        action = { it.userNavigatesToPerson("aSection") }
+                ),
+                NavigationEventsParams(
+                        MainActivityViewState(sectionTitle = "aSection", menuBarEnabled = false, searchEnabled = false),
+                        action = { it.userNavigatesToAbout("aSection") }
+                ),
+                NavigationEventsParams(
+                        MainActivityViewState(sectionTitle = "aSection", menuBarEnabled = false, searchEnabled = false),
+                        action = { it.userNavigatesToLicenses("aSection") }
+                ),
+                NavigationEventsParams(
+                        MainActivityViewState(sectionTitle = "aSection", menuBarEnabled = false, searchEnabled = false),
+                        action = { it.userNavigatesToLicenseContent("aSection") }
+                ),
+                NavigationEventsParams(
+                        MainActivityViewState(sectionTitle = "aSection", menuBarEnabled = false, searchEnabled = false),
+                        action = { it.userNavigatesToAccountDetails("aSection") }
+                )
+        )
     }
 }
