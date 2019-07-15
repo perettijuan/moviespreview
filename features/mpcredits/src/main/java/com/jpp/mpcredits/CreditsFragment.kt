7a6a0030889968b6204c79e71bc3a4986bda90bf
Juan Pablo Peretti <peretti.juan@gmail.com>
2019-07-15 15:55:42 -0300

    Added CreditsPerson and nav to mpcredits

---
+++ features/mpcredits/src/main/java/com/jpp/mpcredits/CreditsFragment.kt
@@ -0,0 +1,14 @@
+package com.jpp.mpcredits
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class CreditsFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.credits_fragment, container, false)
+    }
+}
