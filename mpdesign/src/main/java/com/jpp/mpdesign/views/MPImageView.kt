12b82eae0185e3898356f5a4040a97111ed44936
Juan Pablo Peretti <peretti.juan@gmail.com>
2019-07-18 12:30:39 -0300

    M ale MPImageView extend AppCompatImageView

--- mpdesign/src/main/java/com/jpp/mpdesign/views/MPImageView.kt
+++ mpdesign/src/main/java/com/jpp/mpdesign/views/MPImageView.kt
@@ -2,13 +2,13 @@ package com.jpp.mpdesign.views
 
 import android.content.Context
 import android.util.AttributeSet
-import android.widget.ImageView
+import androidx.appcompat.widget.AppCompatImageView
 import com.jpp.mpdesign.ext.loadImageUrlAsCircular
 
 /**
  * Custom ImageView used mostly to allow loading URLs into the ImageView.
  */
-class MPImageView : ImageView {
+class MPImageView : AppCompatImageView {
 
     constructor(context: Context) : this(context, null)
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
