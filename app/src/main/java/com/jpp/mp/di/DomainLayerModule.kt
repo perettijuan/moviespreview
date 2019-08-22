38115281148b1b72fee70bf86c1812c00746c913
Juan Pablo Peretti <peretti.juan@gmail.com>
2019-08-22 12:30:24 -0300

    Removed ConfigMovieUseCase 2

--- app/src/main/java/com/jpp/mp/di/DomainLayerModule.kt
+++ app/src/main/java/com/jpp/mp/di/DomainLayerModule.kt
@@ -4,19 +4,12 @@ import com.jpp.mpdomain.interactors.ImagesPathInteractor
 import com.jpp.mpdomain.repository.ConfigurationRepository
 import dagger.Module
 import dagger.Provides
-import java.util.concurrent.Executor
-import java.util.concurrent.Executors
-import javax.inject.Singleton
 
 /**
  * Provides all dependencies for the domain layer.
  */
 @Module
 class DomainLayerModule {
-
-    @Provides
-    @Singleton
-    fun providesNetworkExecutor(): Executor = Executors.newFixedThreadPool(5)
 
     @Provides
     fun providesImagesPathInteractor(configurationRepository: ConfigurationRepository)
