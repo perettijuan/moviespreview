161a3f90dbd3360139ddee8bba8d9208cfe14767
Juan Pablo Peretti <peretti.juan@gmail.com>
2019-08-26 16:34:20 -0300

    Updated SearchViewModelTest

--- features/mpsearch/src/test/java/com/jpp/mpsearch/SearchViewModelTest.kt
+++ features/mpsearch/src/test/java/com/jpp/mpsearch/SearchViewModelTest.kt
@@ -1,6 +1,8 @@
 package com.jpp.mpsearch
 
+import android.view.View
 import androidx.lifecycle.MutableLiveData
+import com.jpp.mp.common.coroutines.CoroutineDispatchers
 import com.jpp.mpdomain.interactors.ImagesPathInteractor
 import com.jpp.mpsearch.SearchInteractor.SearchEvent
 import com.jpp.mpsearch.SearchInteractor.SearchEvent.NotConnectedToNetwork
@@ -12,7 +14,10 @@ import io.mockk.impl.annotations.MockK
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.junit5.MockKExtension
 import io.mockk.verify
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -33,8 +38,13 @@ class SearchViewModelTest {
     fun setUp() {
         every { searchInteractor.searchEvents } returns lvInteractorEvents
 
+        val dispatchers = object : CoroutineDispatchers {
+            override fun main(): CoroutineDispatcher = Dispatchers.Unconfined
+            override fun default(): CoroutineDispatcher = Dispatchers.Unconfined
+        }
+
         subject = SearchViewModel(
-                TestSearchCoroutineDispatchers(),
+                dispatchers,
                 searchInteractor,
                 imagesPathInteractor
         )
@@ -48,14 +58,21 @@ class SearchViewModelTest {
     }
 
     @Test
-    fun `Should post ShowSearchView on init`() {
+    fun `Should post clear state on init`() {
         var viewStatePosted: SearchViewState? = null
 
         subject.viewStates.observeWith { it.actionIfNotHandled { viewState -> viewStatePosted = viewState } }
 
         subject.onInit(10)
 
-        assertEquals(SearchViewState.ShowSearchView, viewStatePosted)
+        assertNotNull(viewStatePosted)
+
+        assertEquals(View.INVISIBLE, viewStatePosted?.loadingVisibility)
+        assertEquals(View.INVISIBLE, viewStatePosted?.contentViewState?.searchResultsVisibility)
+        assertEquals(View.INVISIBLE, viewStatePosted?.contentViewState?.emptySearchResultsVisibility)
+        assertEquals(View.INVISIBLE, viewStatePosted?.errorViewState?.visibility)
+
+        assertEquals(View.VISIBLE, viewStatePosted?.placeHolderViewState?.visibility)
     }
 
     @Test
@@ -64,9 +81,19 @@ class SearchViewModelTest {
 
         subject.viewStates.observeWith { it.actionIfNotHandled { viewState -> viewStatePosted = viewState } }
 
+        subject.onSearch("aSearch")
         lvInteractorEvents.postValue(NotConnectedToNetwork)
 
-        assertEquals(SearchViewState.ShowNotConnected, viewStatePosted)
+        assertNotNull(viewStatePosted)
+
+        assertEquals("aSearch", viewStatePosted?.searchQuery)
+        assertEquals(View.INVISIBLE, viewStatePosted?.loadingVisibility)
+        assertEquals(View.INVISIBLE, viewStatePosted?.contentViewState?.searchResultsVisibility)
+        assertEquals(View.INVISIBLE, viewStatePosted?.contentViewState?.emptySearchResultsVisibility)
+        assertEquals(View.INVISIBLE, viewStatePosted?.placeHolderViewState?.visibility)
+
+        assertEquals(View.VISIBLE, viewStatePosted?.errorViewState?.visibility)
+        assertEquals(true, viewStatePosted?.errorViewState?.isConnectivity)
     }
 
     @Test
@@ -75,40 +102,25 @@ class SearchViewModelTest {
 
         subject.viewStates.observeWith { it.actionIfNotHandled { viewState -> viewStatePosted = viewState } }
 
+        subject.onSearch("aSearch")
         lvInteractorEvents.postValue(UnknownError)
 
-        assertEquals(SearchViewState.ShowError, viewStatePosted)
-    }
+        assertNotNull(viewStatePosted)
 
-    @Test
-    fun `Should post loading and create paged list with first movie pages onSearch`() {
-        val viewStatesPosted = mutableListOf<SearchViewState>()
+        assertEquals("aSearch", viewStatePosted?.searchQuery)
+        assertEquals(View.INVISIBLE, viewStatePosted?.loadingVisibility)
+        assertEquals(View.INVISIBLE, viewStatePosted?.contentViewState?.searchResultsVisibility)
+        assertEquals(View.INVISIBLE, viewStatePosted?.contentViewState?.emptySearchResultsVisibility)
+        assertEquals(View.INVISIBLE, viewStatePosted?.placeHolderViewState?.visibility)
 
-        subject.viewStates.observeWith { it.actionIfNotHandled { viewState -> viewStatesPosted.add(viewState) } }
-
-        subject.onSearch("aQuery")
-
-        assertEquals(SearchViewState.ShowSearching, viewStatesPosted[0])
-        verify { searchInteractor.performSearchForPage("aQuery", 1, any()) }
+        assertEquals(View.VISIBLE, viewStatePosted?.errorViewState?.visibility)
+        assertEquals(false, viewStatePosted?.errorViewState?.isConnectivity)
     }
 
     @Test
     fun `Should refresh data when no search has been performed and language changes`() {
+        subject.onSearch("aSearch")
         lvInteractorEvents.value = SearchEvent.AppLanguageChanged
         verify { searchInteractor.flushCurrentSearch() }
     }
-
-    @Test
-    fun `Should post last state when onInit is called for 2nd time`() {
-        var viewStatePosted: SearchViewState? = null
-
-        subject.viewStates.observeWith { it.actionIfNotHandled { viewState -> viewStatePosted = viewState } }
-
-        subject.onInit(10)
-        subject.onSearch("aQuery")
-        subject.onInit(10)
-
-        assertEquals(SearchViewState.ShowSearching, viewStatePosted)
-    }
-
 }
