465295962eea77cc3fdb2d7e7b381e2fa14a265d
Juan Pablo Peretti <peretti.juan@gmail.com>
2019-09-27 12:48:40 -0300

    Navigation uses new navigation arch v2.1

--- app/src/main/java/com/jpp/mp/main/header/NavigationHeaderFragment.kt
+++ app/src/main/java/com/jpp/mp/main/header/NavigationHeaderFragment.kt
@@ -1,37 +1,25 @@
 package com.jpp.mp.main.header
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.jpp.mp.R
-import com.jpp.mp.common.extensions.getViewModel
+import com.jpp.mp.common.extensions.withViewModel
+import com.jpp.mp.common.fragments.MPFragment
 import com.jpp.mp.databinding.FragmentNavHeaderBinding
-import dagger.android.support.AndroidSupportInjection
 import kotlinx.android.synthetic.main.fragment_nav_header.*
-import javax.inject.Inject
 
 /**
  * The application's UI shows a drawer navigation to allow the user redirect the app flow to different
  * places. This particular Fragment takes care of rendering the header of the drawer and allows the
  * user to login into the system or to be redirected to the account details.
  */
-class NavigationHeaderFragment : Fragment() {
-
-    @Inject
-    lateinit var viewModelFactory: ViewModelProvider.Factory
+class NavigationHeaderFragment : MPFragment<NavigationHeaderViewModel>() {
 
     private lateinit var viewBinding: FragmentNavHeaderBinding
-
-    override fun onAttach(context: Context?) {
-        AndroidSupportInjection.inject(this)
-        super.onAttach(context)
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         viewBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_nav_header, container, false)
@@ -55,5 +43,5 @@ class NavigationHeaderFragment : Fragment() {
         }
     }
 
-    private fun withViewModel(action: NavigationHeaderViewModel.() -> Unit) = getViewModel<NavigationHeaderViewModel>(viewModelFactory).action()
+    override fun withViewModel(action: NavigationHeaderViewModel.() -> Unit) = withViewModel<NavigationHeaderViewModel>(viewModelFactory) { action() }
 }
