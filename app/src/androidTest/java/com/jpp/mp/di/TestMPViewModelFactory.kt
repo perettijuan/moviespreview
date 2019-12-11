26e35f3f0946e36daac7bdb40874f5ca689a2207
Juan Pablo Peretti <peretti.juan@gmail.com>
2019-12-11 19:32:44 -0300

    Fix build errors

--- app/src/androidTest/java/com/jpp/mp/di/TestMPViewModelFactory.kt
+++ app/src/androidTest/java/com/jpp/mp/di/TestMPViewModelFactory.kt
@@ -8,7 +8,7 @@ import io.mockk.mockk
 @Suppress("UNCHECKED_CAST")
 class TestMPViewModelFactory(
     private val viewModels: MutableMap<Class<out ViewModel>, ViewModel> = mutableMapOf(),
-    mainViewModel: MainActivityViewModel = MainActivityViewModel(mockk(relaxed = true))
+    mainViewModel: MainActivityViewModel = MainActivityViewModel(mockk(relaxed = true), mockk(relaxed = true))
 ) :
     ViewModelProvider.Factory {
 
