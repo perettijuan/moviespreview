dd04a149f6bd6710d8112d7f8b8c7d5b2e4dbfeb
Juan Pablo Peretti <peretti.juan@gmail.com>
2020-05-25 12:12:24 -0300

    Move MainActivityViewState to its own file

---
+++ app/src/main/java/com/jpp/mp/main/MainActivityViewState.kt
@@ -0,0 +1,10 @@
+package com.jpp.mp.main
+
+/**
+ * Represents the view state that the MainActivity can show at any given time.
+ */
+data class MainActivityViewState(
+        val sectionTitle: String,
+        val menuBarEnabled: Boolean,
+        val searchEnabled: Boolean
+)
