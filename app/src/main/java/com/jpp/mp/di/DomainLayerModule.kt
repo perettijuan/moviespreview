17596fae5fdd4b8481fd4c00823ea02f4f01e32d
Juan Pablo Peretti <peretti.juan@gmail.com>
2020-06-02 16:50:31 -0300

    MovieDetailsViewModel now uses GetMovieDetailsUseCase

--- app/src/main/java/com/jpp/mp/di/DomainLayerModule.kt
+++ app/src/main/java/com/jpp/mp/di/DomainLayerModule.kt
@@ -1,11 +1,9 @@
 package com.jpp.mp.di
 
 import com.jpp.mpdomain.interactors.ImagesPathInteractor
-import com.jpp.mpdomain.repository.ConfigurationRepository
-import com.jpp.mpdomain.repository.ConnectivityRepository
-import com.jpp.mpdomain.repository.LanguageRepository
-import com.jpp.mpdomain.repository.MoviePageRepository
+import com.jpp.mpdomain.repository.*
 import com.jpp.mpdomain.usecase.ConfigureMovieImagesPathUseCase
+import com.jpp.mpdomain.usecase.GetMovieDetailUseCase
 import com.jpp.mpdomain.usecase.GetMoviePageUseCase
 import dagger.Module
 import dagger.Provides
@@ -23,14 +21,23 @@ class DomainLayerModule {
 
     @Provides
     fun providesConfigureMovieImagesPathUseCase(
-            configurationRepository: ConfigurationRepository
+        configurationRepository: ConfigurationRepository
     ): ConfigureMovieImagesPathUseCase = ConfigureMovieImagesPathUseCase(configurationRepository)
 
     @Provides
     fun providesGetMoviePageUseCase(
-            moviePageRepository: MoviePageRepository,
-            connectivityRepository: ConnectivityRepository,
-            languageRepository: LanguageRepository
-    ): GetMoviePageUseCase = GetMoviePageUseCase(moviePageRepository, connectivityRepository, languageRepository)
+        moviePageRepository: MoviePageRepository,
+        connectivityRepository: ConnectivityRepository,
+        languageRepository: LanguageRepository
+    ): GetMoviePageUseCase =
+        GetMoviePageUseCase(moviePageRepository, connectivityRepository, languageRepository)
+
+    @Provides
+    fun providesGetMovieDetailUseCase(
+        movieDetailRepository: MovieDetailRepository,
+        connectivityRepository: ConnectivityRepository,
+        languageRepository: LanguageRepository
+    ): GetMovieDetailUseCase =
+        GetMovieDetailUseCase(movieDetailRepository, connectivityRepository, languageRepository)
 
 }
