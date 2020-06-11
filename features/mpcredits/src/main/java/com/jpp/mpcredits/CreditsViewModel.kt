af8fdf32eb04e35d1ce77fcf760f6321c935d431
Juan Pablo Peretti <peretti.juan@gmail.com>
2020-06-11 19:09:30 -0300

    CreditsViewModel uses GetCreditsUseCase

--- features/mpcredits/src/main/java/com/jpp/mpcredits/CreditsViewModel.kt
+++ features/mpcredits/src/main/java/com/jpp/mpcredits/CreditsViewModel.kt
@@ -6,57 +6,35 @@ import androidx.lifecycle.viewModelScope
 import com.jpp.mp.common.coroutines.MPViewModel
 import com.jpp.mp.common.extensions.addAllMapping
 import com.jpp.mp.common.navigation.Destination
-import com.jpp.mpcredits.CreditsInteractor.CreditsEvent.AppLanguageChanged
-import com.jpp.mpcredits.CreditsInteractor.CreditsEvent.NotConnectedToNetwork
-import com.jpp.mpcredits.CreditsInteractor.CreditsEvent.Success
-import com.jpp.mpcredits.CreditsInteractor.CreditsEvent.UnknownError
 import com.jpp.mpdomain.CastCharacter
 import com.jpp.mpdomain.Credits
 import com.jpp.mpdomain.CrewMember
-import com.jpp.mpdomain.interactors.ImagesPathInteractor
+import com.jpp.mpdomain.usecase.GetCreditsUseCase
+import com.jpp.mpdomain.usecase.Try
 import kotlinx.coroutines.CoroutineDispatcher
-import javax.inject.Inject
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
 /**
  * [MPViewModel] that supports the credits section. The VM retrieves
- * the data from the underlying layers using the provided [CreditsInteractor] and maps the business
+ * the data from the underlying layers and maps the business
  * data to UI data, producing a [CreditsViewState] that represents the configuration of the view
- * at any given moment. The mapping process involves fetching and parsing the paths for the images
- * that each credit possess. This mapping process is executed using the provided [ImagesPathInteractor]
- * to perform the mention images path configuration.
+ * at any given moment.
  *
  * This VM is language aware, meaning that when the user changes the language of the device, the
  * VM is notified about such event and executes a refresh of both: the data stored by the application
  * and the view state being shown to the user.
  */
 class CreditsViewModel @Inject constructor(
-    private val creditsInteractor: CreditsInteractor,
-    private val imagesPathInteractor: ImagesPathInteractor,
+    private val getCreditsUseCase: GetCreditsUseCase,
     private val ioDispatcher: CoroutineDispatcher
 ) : MPViewModel() {
 
     private val _viewState = MediatorLiveData<CreditsViewState>()
-    val viewState: LiveData<CreditsViewState> get() = _viewState
+    val viewState: LiveData<CreditsViewState>  = _viewState
 
     private lateinit var currentParam: CreditsInitParam
-
-    private val retry: () -> Unit = { fetchMovieCredits(currentParam.movieId) }
-
-    /*
-     * Map the business logic coming from the interactor into view layer logic.
-     */
-    init {
-        _viewState.addSource(creditsInteractor.events) { event ->
-            when (event) {
-                is NotConnectedToNetwork -> _viewState.value = _viewState.value?.showNoConnectivityError(retry)
-                is UnknownError -> _viewState.value = _viewState.value?.showUnknownError(retry)
-                is Success -> mapCreditsAndPushViewState(event.credits)
-                is AppLanguageChanged -> refreshCreditsData(currentParam.movieId)
-            }
-        }
-    }
 
     /**
      * Called on VM initialization. The View (Fragment) should call this method to
@@ -67,8 +45,7 @@ class CreditsViewModel @Inject constructor(
     fun onInit(param: CreditsInitParam) {
         currentParam = param
         updateCurrentDestination(Destination.ReachedDestination(currentParam.movieTitle))
-
-        fetchMovieCredits(currentParam.movieId)
+        fetchMovieCredits()
     }
 
     /**
@@ -76,96 +53,70 @@ class CreditsViewModel @Inject constructor(
      */
     fun onCreditItemSelected(personItem: CreditPerson) {
         with(personItem) {
-            navigateTo(Destination.MPPerson(
+            navigateTo(
+                Destination.MPPerson(
                     personId = id.toString(),
                     personImageUrl = profilePath,
-                    personName = subTitle)
+                    personName = subTitle
+                )
             )
         }
     }
 
     /**
      * When called, this method will push the loading view state and will fetch the credits
-     * of the movie being shown. When the fetching process is done, the view state will be updated
-     * based on the result posted by the interactor.
+     * of the movie being shown.
      */
-    private fun fetchMovieCredits(movieId: Double) {
-        withInteractor { fetchCreditsForMovie(movieId) }
+    private fun fetchMovieCredits() {
         _viewState.value = CreditsViewState.showLoading()
-    }
-
-    /**
-     * Starts a refresh data process by indicating to the interactor that new data needs
-     * to be fetched for the credits being shown. This is executed in a background
-     * task while the view state is updated with the loading state.
-     */
-    private fun refreshCreditsData(creditsId: Double) {
-        withInteractor {
-            flushCreditsData()
-            fetchCreditsForMovie(creditsId)
-        }
-        _viewState.value = CreditsViewState.showLoading()
-    }
-
-    /**
-     * Helper function to execute an [action] in the [creditsInteractor] instance
-     * on a background task.
-     */
-    private fun withInteractor(action: CreditsInteractor.() -> Unit) {
         viewModelScope.launch {
-            withContext(ioDispatcher) {
-                action(creditsInteractor)
+            val result = withContext(ioDispatcher) {
+                getCreditsUseCase.execute(currentParam.movieId)
+            }
+
+            when (result) {
+                is Try.Success -> processCreditsResponse(result.value)
+                is Try.Failure -> processFailure(result.cause)
             }
         }
     }
 
-    /**
-     * Maps the [credits] provided from the domain layer to the view layer producing
-     * a new view state that is pushed to the view to show the result of the credits
-     * fetching process.
-     */
-    private fun mapCreditsAndPushViewState(credits: Credits) {
-        when (credits.cast.isEmpty() && credits.crew.isEmpty()) {
-            true -> _viewState.value = _viewState.value?.showNoCreditsAvailable()
-            false -> {
-                viewModelScope.launch {
-                    withContext(ioDispatcher) {
-                        _viewState.value?.showCredits(
-                                credits.cast
-                                        .map { imagesPathInteractor.configureCastCharacter(currentParam.targetImageSize, it) }
-                                        .map { mapCastCharacterToCreditPerson(it) }
-                                        .toMutableList()
-                                        .addAllMapping { credits.crew.map { crewMember -> mapCrewMemberToCreditPerson(crewMember) } }
-                        )
-                    }.let { _viewState.value = it }
-                }
-            }
+    private fun processFailure(failure: Try.FailureCause) {
+        _viewState.value = when (failure) {
+            is Try.FailureCause.NoConnectivity -> _viewState.value?.showNoConnectivityError { fetchMovieCredits() }
+            else -> _viewState.value?.showUnknownError { fetchMovieCredits() }
         }
     }
 
-    /**
-     * Map a [CastCharacter] to a [CreditPerson].
-     */
-    private fun mapCastCharacterToCreditPerson(castCharacter: CastCharacter): CreditPerson =
-            with(castCharacter) {
-                CreditPerson(
-                        id = id,
-                        profilePath = profile_path ?: "empty",
-                        title = character,
-                        subTitle = name
-                )
-            }
+    private fun processCreditsResponse(credits: Credits) {
+        if (credits.cast.isEmpty() && credits.crew.isEmpty()) {
+            _viewState.value = _viewState.value?.showNoCreditsAvailable()
+            return
+        }
 
-    /**
-     * Map a [CrewMember] to a [CreditPerson].
-     */
-    private fun mapCrewMemberToCreditPerson(crewMember: CrewMember): CreditPerson =
-            with(crewMember) {
-                CreditPerson(
-                        id = id,
-                        profilePath = profile_path ?: "empty",
-                        title = name,
-                        subTitle = department
-                )
-            }
+        val creditPersonItems = credits.cast
+            .map { castCharacter -> castCharacter.mapToCreditPerson() }
+            .toMutableList()
+            .addAllMapping { credits.crew.map { crewMember -> crewMember.mapToCreditPerson() } }
+
+        _viewState.value = _viewState.value?.showCredits(creditPersonItems)
+    }
+
+    private fun CastCharacter.mapToCreditPerson(): CreditPerson {
+        return CreditPerson(
+            id = id,
+            profilePath = profile_path ?: "empty",
+            title = character,
+            subTitle = name
+        )
+    }
+
+    private fun CrewMember.mapToCreditPerson(): CreditPerson {
+        return CreditPerson(
+            id = id,
+            profilePath = profile_path ?: "empty",
+            title = name,
+            subTitle = department
+        )
+    }
 }
