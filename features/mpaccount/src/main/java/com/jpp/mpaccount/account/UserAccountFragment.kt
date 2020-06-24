97aad9fad9a6a6de5e4de0649de4ba60afe0a220
Juan Pablo Peretti <peretti.juan@gmail.com>
2020-06-24 19:07:59 -0300

    Cleanup UserAccountFragment - first step

--- features/mpaccount/src/main/java/com/jpp/mpaccount/account/UserAccountFragment.kt
+++ features/mpaccount/src/main/java/com/jpp/mpaccount/account/UserAccountFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.CookieManager
+import android.widget.Button
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import com.jpp.mp.common.extensions.getScreenWidthInPixels
@@ -12,7 +13,6 @@ import com.jpp.mp.common.extensions.withViewModel
 import com.jpp.mp.common.fragments.MPFragment
 import com.jpp.mpaccount.R
 import com.jpp.mpaccount.databinding.FragmentUserAccountBinding
-import kotlinx.android.synthetic.main.layout_user_account_content.*
 
 /**
  * Fragment used to show the user's account data. It provides the user the ability to logout of the
@@ -22,39 +22,57 @@ class UserAccountFragment : MPFragment<UserAccountViewModel>() {
 
     private lateinit var viewBinding: FragmentUserAccountBinding
 
+    private var userAccountLogoutBtn: Button? = null
+    private var userAccountFavoriteMovies: UserAccountMoviesView? = null
+    private var userAccountRatedMovies: UserAccountMoviesView? = null
+    private var userAccountWatchlist: UserAccountMoviesView? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         viewBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_user_account, container, false)
         return viewBinding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        userAccountLogoutBtn = view.findViewById(R.id.userAccountLogoutBtn)
+        userAccountFavoriteMovies = view.findViewById(R.id.userAccountFavoriteMovies)
+        userAccountRatedMovies = view.findViewById(R.id.userAccountRatedMovies)
+        userAccountWatchlist = view.findViewById(R.id.userAccountWatchlist)
+
         withViewModel {
             viewState.observe(this@UserAccountFragment.viewLifecycleOwner, Observer { viewState ->
                 viewBinding.viewState = viewState
             })
             onInit(UserAccountParam.create(
-                    resources,
-                    getScreenWidthInPixels()
+                resources,
+                getScreenWidthInPixels()
             ))
         }
 
-        userAccountLogoutBtn.setOnClickListener {
+        userAccountLogoutBtn?.setOnClickListener {
             withViewModel { onLogout() }
             CookieManager.getInstance().removeAllCookies(null)
         }
 
-        userAccountFavoriteMovies.setOnClickListener {
+        userAccountFavoriteMovies?.setOnClickListener {
             withViewModel { onFavorites() }
         }
 
-        userAccountRatedMovies.setOnClickListener {
+        userAccountRatedMovies?.setOnClickListener {
             withViewModel { onRated() }
         }
 
-        userAccountWatchlist.setOnClickListener {
+        userAccountWatchlist?.setOnClickListener {
             withViewModel { onWatchlist() }
         }
+    }
+
+    override fun onDestroyView() {
+        userAccountLogoutBtn = null
+        userAccountFavoriteMovies = null
+        userAccountRatedMovies = null
+        userAccountWatchlist = null
+        super.onDestroyView()
     }
 
     override fun withViewModel(action: UserAccountViewModel.() -> Unit) = withViewModel<UserAccountViewModel>(viewModelFactory) { action() }
