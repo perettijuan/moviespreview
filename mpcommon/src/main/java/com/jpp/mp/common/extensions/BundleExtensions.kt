7f4e2628294098c85a8debba92a3d8cefb9dc6da
Juan Pablo Peretti <peretti.juan@gmail.com>
2020-06-29 12:14:55 -0300

    general cleanups

--- mpcommon/src/main/java/com/jpp/mp/common/extensions/BundleExtensions.kt
+++ mpcommon/src/main/java/com/jpp/mp/common/extensions/BundleExtensions.kt
@@ -2,13 +2,6 @@ package com.jpp.mp.common.extensions
 
 import android.os.Bundle
 
-/**
- * Finds the provided [key] in the Bundle. If the key does not
- * exists (or the Bundle is null), [default] String is returned.
- */
-fun Bundle?.getStringOrDefault(key: String, default: String = ""): String {
-    return this?.let { getString(key) } ?: default
-}
 
 fun Bundle?.getStringOrFail(key: String): String {
     return this?.let { getString(key) } ?: throw IllegalStateException("Can't find the provided $key in this Bundle")
--- mpcommon/src/main/java/com/jpp/mp/common/extensions/FragmentExtensions.kt
+++ mpcommon/src/main/java/com/jpp/mp/common/extensions/FragmentExtensions.kt
@@ -2,45 +2,6 @@ package com.jpp.mp.common.extensions
 
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-
-/**
- * Returns an integer that represents the width of the screen in pixels.
- */
-fun Fragment.getScreenWidthInPixels(): Int {
-    activity?.let {
-        return it.getScreenSizeInPixels().x
-    } ?: throw IllegalStateException("Activity is null at this øpoint")
-}
-
-/**
- * Retrieves the identifier of a given attribute defined in the Activity's theme.
- */
-fun Fragment.getResIdFromAttribute(attr: Int): Int {
-    val tp = android.util.TypedValue()
-    activity?.theme?.resolveAttribute(attr, tp, true)
-    return tp.resourceId
-}
-
-/**
- * Extension function to find a ViewModel in the Activity of the Fragment.
- */
-inline fun <reified T : ViewModel> Fragment.getViewModel(viewModelFactory: ViewModelProvider.Factory): T {
-    return activity?.run {
-        ViewModelProvider(this, viewModelFactory)[T::class.java]
-    } ?: throw RuntimeException("Invalid Activity")
-}
-
-/**
- * Extension function to simplify the access to a ViewModel backed by the Fragment's
- * Activity.
- */
-inline fun <reified T : ViewModel> Fragment.withViewModel(viewModelFactory: ViewModelProvider.Factory, body: T.() -> Unit): T {
-    val vm = getViewModel<T>(viewModelFactory)
-    vm.body()
-    return vm
-}
 
 /**
  * Changes the title of the screen to the new provided [title].
