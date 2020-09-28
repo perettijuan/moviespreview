6fa7a1dc0df6a597c35e6ac9ad0fcfe799c6b742
Juan Pablo Peretti <peretti.juan@gmail.com>
2020-09-28 18:23:12 -0300

    Inject DiscoverMoviesFragment

--- app/src/main/java/com/jpp/mp/main/discover/DiscoverMoviesFragment.kt
+++ app/src/main/java/com/jpp/mp/main/discover/DiscoverMoviesFragment.kt
@@ -1,13 +1,33 @@
 package com.jpp.mp.main.discover
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.jpp.mp.R
+import com.jpp.mp.common.viewmodel.MPGenericSavedStateViewModelFactory
+import dagger.android.support.AndroidSupportInjection
+import javax.inject.Inject
 
 class DiscoverMoviesFragment : Fragment() {
+
+    @Inject
+    lateinit var viewModelFactory: DiscoverMoviesViewModelFactory
+
+    private val viewModel: DiscoverMoviesViewModel by viewModels {
+        MPGenericSavedStateViewModelFactory(
+            viewModelFactory,
+            this
+        )
+    }
+
+    override fun onAttach(context: Context) {
+        AndroidSupportInjection.inject(this)
+        super.onAttach(context)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,5 +35,11 @@ class DiscoverMoviesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_discover_movies, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.onInit()
     }
 }
