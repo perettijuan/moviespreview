1e5c4cae1e10be4a7f061d8245fb4887d93803ee
Juan Pablo Peretti <peretti.juan@gmail.com>
2020-10-05 22:00:11 -0300

    Yet another cleanup

--- app/src/main/java/com/jpp/mp/main/discover/DiscoverMoviesFragment.kt
+++ app/src/main/java/com/jpp/mp/main/discover/DiscoverMoviesFragment.kt
@@ -21,7 +21,6 @@ import com.jpp.mp.main.discover.filters.genres.GenreFilterItem
 import dagger.android.support.AndroidSupportInjection
 import javax.inject.Inject
 
- *  selector for apply button
 class DiscoverMoviesFragment : Fragment() {
 
     @Inject
